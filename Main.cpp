0030f322e540b279626c78f0f0bbeba59c86d7f4
Pradyuman Yadav <pradyuman098@gmail.com>
2026-08-10 02:08:10 +0530

    Basic Opperator Calculator with Error Dettection in Opperator

--- Main.cpp
+++ Main.cpp
@@ -1,47 +1,60 @@
 #include<iostream>
 #include<cmath>
 
+void ask2num (long double &a,long double &b){
+    std::cout << "Enter the first number: " ;
+            std::cin >> a ;
+    std::cout << "Enter the Second number: " ;
+            std::cin >> b ;
+}
+
+bool validate_op(char &a){
+    return (a=='+'|| a=='-'|| a=='*'|| a=='/'|| a=='%');
+}
+
 int main (){
 
     char op ;
-    double num1 ;
-    double num2 ;
-    double res  ;    
+    long double num1, num2;
 
     std::cout << "-------------------------------------- CALCULATOR -------------------------------------- \n" ;
-        std::cout << "Enter the opperation to be done (Enter only +,-,*,/,%): "  ;
-            std::cin >> op ;
-        std::cout << "Enter the first number: " ;
-            std::cin >> num1 ;
-        std::cout << "Enter the first number: " ;
-            std::cin >> num2 ;
+        
 
-        switch (op)
-        {
-        case '+':
+        do{
+            std::cout << "Enter the opperation to be done (Enter only +,-,*,/,%): "  ;
+            std::cin >> op ;
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                if (not(validate_op (op))){
+                   std::cout << "The Entered Opperation is Invalid! \n Try again \n \n";
+                }
+        } while (not (validate_op(op)));
+    
+        ask2num(num1,num2);
+                
+        switch (op){
+            case '+':
                 std::cout << "The sum of the Numbers " << num1 << " and " << num2 << " is " << num1+num2 << '\n'; 
             break;
 
-        case '-':
+            case '-':
                 std::cout << "The difference of the Numbers " << num1 << " and " << num2 << " is " << num1-num2 << '\n'; 
             break;    
         
-        case '*':
+            case '*':
                 std::cout << "The product of the Numbers " << num1 << " and " << num2 << " is " << num1*num2 << '\n'; 
             break;  
 
-        case '/':
+            case '/':
                 std::cout << "The quotient of the Numbers " << num1 << " and " << num2 << " is " << num1/num2 << '\n'; 
             break;
 
-        case '%':
+            case '%':
                 std::cout << "The Modulo/Remainder of the Numbers " << num1 << " and " << num2 << " is " << std::fmod(num1, num2)
-                                                                                                            << '\n'; 
+                                                                                                                        << '\n'; 
             break;    
-
-        default:
-            break;
         }
+            
+        
 
 
 
