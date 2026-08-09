5f83ff41f4c6a21c20c855f7da8dbefc27442570
Pradyuman Yadav <pradyuman098@gmail.com>
2026-08-10 00:28:38 +0530

    calculator With basic oprators

--- Main.cpp
+++ Main.cpp
@@ -21,7 +21,24 @@ int main (){
         case '+':
                 std::cout << "The sum of the Numbers " << num1 << " and " << num2 << " is " << num1+num2 << '\n'; 
             break;
+
+        case '-':
+                std::cout << "The difference of the Numbers " << num1 << " and " << num2 << " is " << num1-num2 << '\n'; 
+            break;    
         
+        case '*':
+                std::cout << "The product of the Numbers " << num1 << " and " << num2 << " is " << num1*num2 << '\n'; 
+            break;  
+
+        case '/':
+                std::cout << "The quotient of the Numbers " << num1 << " and " << num2 << " is " << num1/num2 << '\n'; 
+            break;
+
+        case '%':
+                std::cout << "The Modulo/Remainder of the Numbers " << num1 << " and " << num2 << " is " << std::fmod(num1, num2)
+                                                                                                            << '\n'; 
+            break;    
+
         default:
             break;
         }
