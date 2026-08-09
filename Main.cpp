0663a7144618583cdac9df6469354a9daca437c2
Pradyuman Yadav <pradyuman098@gmail.com>
2026-08-09 23:46:14 +0530

    Calculator only + version

--- Main.cpp
+++ Main.cpp
@@ -3,5 +3,32 @@
 
 int main (){
 
+    char op ;
+    double num1 ;
+    double num2 ;
+    double res  ;    
+
+    std::cout << "-------------------------------------- CALCULATOR -------------------------------------- \n" ;
+        std::cout << "Enter the opperation to be done (Enter only +,-,*,/,%): "  ;
+            std::cin >> op ;
+        std::cout << "Enter the first number: " ;
+            std::cin >> num1 ;
+        std::cout << "Enter the first number: " ;
+            std::cin >> num2 ;
+
+        switch (op)
+        {
+        case '+':
+                std::cout << "The sum of the Numbers " << num1 << " and " << num2 << " is " << num1+num2 << '\n'; 
+            break;
+        
+        default:
+            break;
+        }
+
+
+
+    std::cout << "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx \n" ;
+
     
 }
